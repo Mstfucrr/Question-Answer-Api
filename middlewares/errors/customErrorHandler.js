78a53d0bf0649d10f0ceb1bd6e65365998e265be
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-01-20 00:36:53 +0300

    customErrorHandler update

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -1,8 +1,18 @@
+const CustomError = require('../../helpers/error/CustomError')
 const customErrorHandler = (err, req, res, next) => {
-    console.log(err);
+    let customError = err;
+    if (err.name === 'SyntaxError') {
+        customError = new CustomError("Syntax err", 400)
+    }
+    else if (err.name === 'ValidationError') {
+        customError = new CustomError(err.message, 400)
+    }
     res
-        .status(400)
-        .json({ success: false })
+        .status(customError.status || 500)
+        .json({
+            success: false,
+            message: customError.message
+        })
 }
 
 module.exports = customErrorHandler
