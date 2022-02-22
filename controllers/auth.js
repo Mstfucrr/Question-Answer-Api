a52cadcd0b484dfc321c1096b448b6d16c013464
mustafa uçar <mustafa_asus@hotmail.com>
2022-02-22 21:30:15 +0300

    add login and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 const User = require("../models/User")
-const asyncHandler = require("express-async-handler")
+const asyncErrorWrapper = require("express-async-handler")
 const { sendJwtToClient } = require("../helpers/authorization/tokenHelpers")
+const { validateUserInput, comparePasword } = require("../helpers/input/inputHelpers")
 const CustomError = require("../helpers/error/CustomError")
 
-const register = asyncHandler(async (req, res, next) => {
+const register = asyncErrorWrapper(async (req, res, next) => {
     const { name, email, password, role } = req.body
     const user = await User.create({
         name,
@@ -14,16 +15,41 @@ const register = asyncHandler(async (req, res, next) => {
     sendJwtToClient(user, res)
 });
 
+const login = asyncErrorWrapper(async (req, res, next) => {
+    const { email, password } = req.body
+    if (!validateUserInput(email, password))
+        return next(new CustomError("Please check your inputs", 400));
+
+    const user = await User.findOne({ email }).select("+password")
+
+    if (!user || !comparePasword(password, user.password)) {
+        return next(new CustomError("Please check your email or password", 400));
+    }
+    sendJwtToClient(user, res)
+})
+
+const logout = asyncErrorWrapper(async (req, res, next) => {
+    const { NODE_ENV } = process.env
+    return res.status(200).cookie({
+        httpOnly: true,
+        expires: new Date(Date.now()),
+        secure: NODE_ENV === "development" ? false : true
+    }).json({
+        success: true,
+        message: "You have been logged out"
+    });
+})
+
 const getUser = (req, res, next) => {
     res.json({
         success: true,
         data: {
-            id: req.user.id, 
-            name : req.user.name
+            id: req.user.id,
+            name: req.user.name
         }
     })
 }
 
 module.exports = {
-    register, getUser
+    register, login, logout, getUser
 };
