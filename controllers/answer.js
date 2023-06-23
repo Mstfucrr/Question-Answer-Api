52f5be305ab635e508be71304f7445ea189a831d
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-24 01:04:39 +0300

    add answer controller functions

---
+++ controllers/answer.js
@@ -0,0 +1,155 @@
+const Answer = require('../models/Answer');
+const Question = require('../models/Question');
+const CustomError = require('../helpers/error/CustomError');
+const asyncErrorWrapper = require('express-async-handler');
+
+
+const addNewAnswerToQuestion = asyncErrorWrapper(async (req, res, next) => {
+
+    const { question_id } = req.params;
+    const user_id = req.user.id;
+    const information = req.body;
+
+    const answer = await Answer.create({
+        ...information,
+        question: question_id,
+        user: user_id
+    });
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: answer,
+            message: "Answer added successfully"
+        });
+
+
+});
+
+
+const getAllAnswersByQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { question_id } = req.params;
+
+    const question = await Question.findById(question_id).populate("answers"); // question_id'ye göre question'ı bul ve question'ın answers'larını getir
+
+    const answers = question.answers;
+
+    return res.status(200)
+        .json({
+            success: true,
+            count: answers.length,
+            data: answers
+        })
+
+});
+
+const getSingleAnswer = asyncErrorWrapper(async (req, res, next) => {
+    const { answer_id } = req.params;
+
+    const answer = await Answer.findById(answer_id)
+        .populate({
+            path: "question",
+            select: ["title", "content"]
+        })
+        .populate({
+            path: "user",
+            select: "name profile_image"
+        });
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: answer
+        })
+
+});
+
+const editAnswer = asyncErrorWrapper(async (req, res, next) => {
+    const { answer_id } = req.params;
+
+    const { content } = req.body;
+
+    const answer = await Answer.findById(answer_id);
+
+    answer.content = content;
+
+    await answer.save();
+
+    return res.status(200)
+        .json({
+            success: true,
+            message: "Answer edited successfully",
+            data: answer
+        })
+});
+
+const deleteAnswer = asyncErrorWrapper(async (req, res, next) => {
+    const { answer_id } = req.params;
+    const { question_id } = req.params;
+
+    const question = await Question.findById(question_id);
+    await Answer.findByIdAndRemove(answer_id)
+        .then(async () => {
+            question.answers.splice(question.answers.indexOf(answer_id), 1);
+            await question.save();
+        }).then(() => {
+            return res.status(200)
+                .json({
+                    success: true,
+                    message: "Answer deleted successfully"
+                })
+        }).catch(err => {
+            return next(new CustomError("Answer couldn't be deleted", 400));
+        })
+
+});
+
+const likeAnswer = asyncErrorWrapper(async (req, res, next) => {
+    const { answer_id } = req.params;
+    const answer = await Answer.findById(answer_id);
+
+    if (answer.likes.includes(req.user.id)) {
+        return next(new CustomError("You already liked this answer", 400));
+    }
+
+    answer.likes.push(req.user.id);
+    await answer.save();
+
+    return res.status(200)
+        .json({
+            success: true,
+            message: "Answer liked successfully"
+        })
+});
+
+const undoLikeAnswer = asyncErrorWrapper(async (req, res, next) => {
+    const { answer_id } = req.params;
+
+    const answer = await Answer.findById(answer_id);
+
+    if (!answer.likes.includes(req.user.id)) {
+        return next(new CustomError("You can't undo like operation for this answer", 400));
+    }
+
+    const index = answer.likes.indexOf(req.user.id);
+    answer.likes.splice(index, 1);
+    await answer.save();
+
+    return res.status(200)
+        .json({
+            success: true,
+            message: "Undo like operation is successful"
+        })
+});
+
+
+
+module.exports = {
+    addNewAnswerToQuestion,
+    getAllAnswersByQuestion,
+    getSingleAnswer,
+    editAnswer,
+    deleteAnswer,
+    likeAnswer,
+    undoLikeAnswer
+}
