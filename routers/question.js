182edcf89d4ae1b6fa50b484bded82c7847d0964
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-23 22:41:53 +0300

    add quesiton routers

--- routers/question.js
+++ routers/question.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getAllQuestions } = require('../controllers/question')
+const { askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, deleteQuestion } = require('../controllers/question')
+const { getAccessToRoute , getQuestionOwnerAccess } = require('../middlewares/authorization/auth')
+const { checkQuestionExist } = require('../middlewares/database/databaseErrorHelpers')
+
 
 router.get('/', getAllQuestions)
+router.get('/:id', checkQuestionExist, getSingleQuestion)
+
+router.post('/ask', getAccessToRoute, askNewQuestion)
+router.put('/:id/edit', [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], editQuestion)
+// delete
+router.delete('/:id/delete', [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], deleteQuestion)
+
 
 module.exports = router
