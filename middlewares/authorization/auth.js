680392bcf8b153c6824fa68658da984bb56224c1
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-23 19:37:52 +0300

    add getAdminAccess

--- middlewares/authorization/auth.js
+++ middlewares/authorization/auth.js
@@ -1,25 +1,40 @@
 const CustomError = require('../../helpers/error/CustomError')
 const jwt = require('jsonwebtoken')
 const { isTokenInculed, getAccessTokenFromHeaders } = require('../../helpers/authorization/tokenHelpers')
+const asyncErrorWrapper = require('express-async-handler')
+const User = require('../../models/User')
 
-const getAccessToRoute = (req, res, next) => {
-    const { JWT_SECRET_KEY } = process.env
-    if (!isTokenInculed(req)) {
-        return next(new CustomError("You are not authorization to access this route", 401));
+const getAccessToRoute = (req, res, next) => { 
+    const { JWT_SECRET_KEY } = process.env //env'den secret key'i al
+    if (!isTokenInculed(req)) { //token yoksa
+        return next(new CustomError("You are not authorization to access this route", 401)); //hata döndür
     }
-    const accessToken = getAccessTokenFromHeaders(req)
-    jwt.verify(accessToken, JWT_SECRET_KEY, (err, decoded) => {
-        if (err) {
-            return next(new CustomError("You are not authorization to access this route", 401));
+    const accessToken = getAccessTokenFromHeaders(req) //token varsa al
+    jwt.verify(accessToken, JWT_SECRET_KEY, (err, decoded) => { //token'ı verify et
+        if (err) { //token geçersizse
+            return next(new CustomError("You are not authorization to access this route", 401)); //hata döndür
         }
-        req.user = {
+        req.user = { //token geçerliyse user'ı al
             id: decoded.id,
             name: decoded.name
         }
-        next();
+        next(); //devam et
     })
 }
 
+const getAdminAccess = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.user //user'ın id'sini al
+
+    const user = await User.findById(id) //id'ye göre user'ı bul
+    if (user.role !== "admin") { //admin değilse
+        return next(new CustomError("Only admins can access this route", 403))
+    } //admin değilse hata döndür
+    next(); //adminse devam et
+
+})
+
+
 module.exports = {
-    getAccessToRoute
+    getAccessToRoute,
+    getAdminAccess
 }
