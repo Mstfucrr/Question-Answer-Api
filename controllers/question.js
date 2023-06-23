14ed2e1062bdc58a34051d6580ca06a792839873
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-23 22:40:57 +0300

    add askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, deleteQuestion functions

--- controllers/question.js
+++ controllers/question.js
@@ -1,9 +1,82 @@
-const getAllQuestions = (req, res, next) => {
-    res
-    .status(200)
-    .json({ success: true })
-}
+const Question = require('../models/Question');
+const CustomError = require('../helpers/error/CustomError');
+const asyncErrorWrapper = require('express-async-handler');
+
+const askNewQuestion = asyncErrorWrapper(async (req, res, next) => {
+
+    const information = req.body;
+
+    const question = await Question.create({
+        ...information,
+        user: req.user.id
+    });
+
+    res.status(200)
+        .json({
+            success: true,
+            data: question
+        });
+});
+
+const getAllQuestions = asyncErrorWrapper(async (req, res, next) => {
+
+    const questions = await Question.find();
+    return res.status(200)
+        .json({
+            success: true,
+            data: questions
+
+        })
+        
+});
+
+const getSingleQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    const question = await Question.findById(id);
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: question
+        })
+});
+
+const editQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+    const information = req.body;
+
+    let question = await Question.findById(id);
+    question.title = information.title || question.title; // eğer title yoksa eski title'ı kullan
+    question.content = information.content || question.content; // eğer content yoksa eski content'ı kullan
+
+    question = await question.save();
+    
+    return res.status(200)
+        .json({
+            success: true,
+            data: question
+        })
+});
+
+const deleteQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    await Question.findByIdAndDelete(id);
+
+    return res.status(200)
+        .json({
+            success: true,
+            message: "Question deleted successfully"
+        })
+});
+
+
 
 module.exports = {
-    getAllQuestions
+    askNewQuestion,
+    getAllQuestions,
+    getSingleQuestion,
+    editQuestion,
+    deleteQuestion
 };
