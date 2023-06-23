fc73672fdfea64b9d10baebd2a95c772bb23cfa0
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-23 18:03:49 +0300

    add custom error "CastError" for for invalid ids

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -7,6 +7,9 @@ const customErrorHandler = (err, req, res, next) => {
     if (err.name === 'ValidationError') {
         customError = new CustomError(err.message, 400)
     }
+    if (err.name === 'CastError') {
+        customError = new CustomError("Please provide a valid id", 400)
+    }
     if (err.code === 11000) {
         customError = new CustomError("E-mail is already in use",400)
     }
