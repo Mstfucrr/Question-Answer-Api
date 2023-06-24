d0b6e3dfb68daf95474dfe8f0443dc507077dbe7
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-24 19:33:47 +0300

    fix imports

--- routers/answer.js
+++ routers/answer.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router({ mergeParams: true }); // mergeParams: true ile question_id'yi answer.js'e taşıyoruz. Böylece answer.js'de question_id'yi kullanabiliyoruz
 const { getAccessToRoute, getAnswerOwnerAccess } = require('../middlewares/authorization/auth'); 
 const { addNewAnswerToQuestion, getAllAnswersByQuestion, getSingleAnswer, editAnswer, deleteAnswer, likeAnswer, undoLikeAnswer } = require('../controllers/answer');
-const { checkQuestionExist,checkQuestionAndAnswerExist } = require('../middlewares/database/databaseErrorHelpers');
+const { checkQuestionAndAnswerExist } = require('../middlewares/database/databaseErrorHelpers');
+
+
 
 router.post('/', [getAccessToRoute], addNewAnswerToQuestion);
 router.get('/', getAllAnswersByQuestion);
