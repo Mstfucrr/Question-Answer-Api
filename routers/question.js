cafc5fc79815a64d35b10adf0b63244adc4e8924
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-24 15:29:57 +0300

    add questionQueryMiddleware to getAllQuestions router

--- routers/question.js
+++ routers/question.js
@@ -4,15 +4,24 @@ const { askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, delete
 const { getAccessToRoute , getQuestionOwnerAccess } = require('../middlewares/authorization/auth')
 const { checkQuestionExist } = require('../middlewares/database/databaseErrorHelpers')
 const answer = require('./answer')
+const Question = require('../models/Question')
+const {questionQueryMiddleware} = require('../middlewares/query/questionQueryMiddleware.js')
 
-router.get('/', getAllQuestions)
+
+
+router.get('/',
+    questionQueryMiddleware(Question, {
+        population: {
+            path: 'user',
+            select: 'name profile_image'
+        }
+    }),getAllQuestions)
 router.get('/:id', checkQuestionExist, getSingleQuestion)
 
 // add, edit and delete question
 router.post('/ask', getAccessToRoute, askNewQuestion)
 router.put('/:id/edit', [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], editQuestion)
 router.delete('/:id/delete', [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], deleteQuestion)
-
 // like and undo like question
 router.get('/:id/like', [getAccessToRoute, checkQuestionExist], likeQuestion)
 router.get('/:id/undo_like', [getAccessToRoute, checkQuestionExist], undoLikeQuestion)
