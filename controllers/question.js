a5756ccdba492b6d8487309ab6e0f2bc6df951e4
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-24 19:33:24 +0300

    fix call queryResults

--- controllers/question.js
+++ controllers/question.js
@@ -25,15 +25,8 @@ const getAllQuestions = asyncErrorWrapper(async (req, res, next) => {
 });
 
 const getSingleQuestion = asyncErrorWrapper(async (req, res, next) => {
-    const { id } = req.params;
-
-    const question = await Question.findById(id);
-
     return res.status(200)
-        .json({
-            success: true,
-            data: question
-        })
+        .json(res.queryResults);
 });
 
 const editQuestion = asyncErrorWrapper(async (req, res, next) => {
