8206d8b73848116c01a27639977419d25739c211
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-06-24 19:34:31 +0300

    add userQueryMiddleware

--- routers/user.js
+++ routers/user.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router();
 const { getSingleUser, getAllUsers } = require('../controllers/user')
 const { chechUserExist } = require('../middlewares/database/databaseErrorHelpers')
-
+const { userQueryMiddleware } = require('../middlewares/query/userQueryMiddleware');
+const User = require('../models/User');
 // tüm user'ları getir
-router.get('/', getAllUsers) //tüm user'ları getir
+
+
+router.get('/', userQueryMiddleware(User), getAllUsers) //tüm user'ları getir
 router.get('/:id', chechUserExist, getSingleUser) //id'ye göre user var mı yok mu kontrol et, varsa getSingleUser'a git ve user'ı getir
 
 
