4a6727c403216e6f35d4483a8b4895f6880747ec
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-02 01:23:11 +0300

    add CORS

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+        return res.status(200).json({})
+    }
+    next()
+})
 app.use("/api", routers)
+// cross allow
 
 
 app.get("/", (req, res) => {
